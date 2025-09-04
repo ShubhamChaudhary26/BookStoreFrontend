db23bba8d22995fdc118fc849b548c64dc734f98
Shubham Chaudhary <sc3617378@gmail.com>
2025-09-04 13:38:00 +0530

    api call on render

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api", 
-});
+const API = axios.create({ baseURL: "https://bookstorebackend-8ke2.onrender.com/api" });
+
 
 // Token middleware
 API.interceptors.request.use((req) => {
